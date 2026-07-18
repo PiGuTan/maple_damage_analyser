6032068f4b69622a91c6af9072704854ca896624
PiGuTan <pigutheboss@gmail.com>
2026-07-18 12:56:12 +0800

    initial commit

---
+++ ba_extractor.py
@@ -0,0 +1,20 @@
+import easyocr
+import cv2
+import re
+import objects
+import argparse
+
+parser = argparse.ArgumentParser(parents=[objects.video_parser, objects.frame_parser])
+parser.add_argument("--file", type=str, help="path to video file")
+args = parser.parse_args()
+
+def main():
+    video = objects.Video(args.file)
+    for i in video.frames:
+        frame = objects.Frame(video.get_frame(i), video.get_frame_time(i))
+
+        frame.crop_frame()
+        frame.read_frame()
+
+if __name__ == "__main__":
+    main()
